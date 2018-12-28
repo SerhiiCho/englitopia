f6c01f87d7d28d59c9957b7d81168f375c6c2696
SerhiiCho <serhiicho@inbox.ru>
2018-12-29 00:09:39 +0200

    -1- Changed dbh.inc.php

--- build/includes/dbh.inc.php
+++ build/includes/dbh.inc.php
@@ -7,5 +7,5 @@ require_once(BASE_DIR . '/libs/rb.php');
 require_once(BASE_DIR . '/config.php');
 
 // Database connection
-R::setup( 'mysql:host=' . DB_HOST.';dbname=' . DB_NAME, DB_USER, DB_PASSWORD );
-R::freeze( true );
+R::setup('mysql:host=' . DB_HOST.';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+R::freeze(true);
