681af5bd0c306c5d5099fef437f330b08bfef0ef
SerhiiCho <serhiicho@inbox.ru>
2018-12-29 11:33:23 +0200

    -1- renamed build folder to website

--- website/includes/dbh.inc.php
+++ website/includes/dbh.inc.php
@@ -1,10 +1,12 @@
-<?php session_start();
+<?php
+
+session_start();
 
 // Define base params
 define('BASE_DIR', dirname(dirname(__FILE__)));
 
 require_once(BASE_DIR . '/libs/rb.php');
-require_once(BASE_DIR . '/../config.php');
+require_once(BASE_DIR . '/config.php');
 
 // Database connection
 R::setup('mysql:host=' . DB_HOST.';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
